3af2010c5286461ea667726248c2630136331750
Passion H <phood419@gmail.com>
2025-04-29 15:11:03 -0400

    code

--- StreamlitAppFinal/portfolio_analyzer_app.py
+++ StreamlitAppFinal/portfolio_analyzer_app.py
@@ -184,5 +184,5 @@ if uploaded_file:
     except Exception as e:
         st.error(f"An error occurred: {e}")
         st.warning("Please check your CSV file format and try again.")
-else: 
-     st.info('👈 Upload a CSV file to get started!')
+else:
+    st.info('👈 Upload a CSV file to get started!')
